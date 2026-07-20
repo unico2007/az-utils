c7d9ba667b4675c834e5c20847dba8f852ae4a31
Murad Farajzada <mfarajzadaa@gmail.com>
2026-07-20 14:29:26 +0400

    feat(dates): add isValidDateParts (#89)
    Validates a year/month/day triple as a real calendar date.

--- src/dates/index.ts
+++ src/dates/index.ts
@@ -2,6 +2,7 @@ export { getAge } from "./age";
 export { daysBetween } from "./daysBetween";
 export { formatDateAz, MONTH_NAMES_AZ } from "./months";
 export { quarterOf } from "./quarter";
+export { isValidDateParts } from "./validParts";
 export { WEEKDAY_NAMES_AZ, weekdayNameAz } from "./weekdays";
 export { isWeekend, isWorkingDay } from "./workday";
 export { addWorkingDays, workingDaysBetween } from "./workdayCalc";
