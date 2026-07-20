bb2bb1edf09aba37d4e08531f2eb9b0271da7c4d
Murad Farajzada <mfarajzadaa@gmail.com>
2026-07-20 14:25:53 +0400

    feat(text): add initials (#85)
    Builds uppercase initials from a name (Azerbaijani-aware).

--- src/text/index.ts
+++ src/text/index.ts
@@ -1,5 +1,6 @@
 export { asciiFold } from "./asciiFold";
 export { capitalizeFirstAz } from "./capitalizeFirst";
 export { countWords } from "./countWords";
+export { initials } from "./initials";
 export { normalizeSpaces } from "./normalizeSpaces";
 export { truncate } from "./truncate";
