f4b77e3c4fb1f5750a33e2861af1834d96eb7ad0
Murad Farajzada <mfarajzadaa@gmail.com>
2026-07-20 14:41:12 +0400

    feat(number): add isEven (#97)

--- src/number/index.ts
+++ src/number/index.ts
@@ -2,6 +2,7 @@ export { average } from "./average";
 export { clamp } from "./clamp";
 export { formatNumberAz } from "./format";
 export { inRange } from "./inRange";
+export { isEven } from "./isEven";
 export { parseNumberAz } from "./parse";
 export { roundTo } from "./round";
 export { sum } from "./sum";
