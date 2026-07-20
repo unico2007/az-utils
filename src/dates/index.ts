51239252e45aab5099f5421e8a876c7fb9dd24f1
farajzada <mfarajzadaa@gmail.com>
2026-07-20 14:28:18 +0400

    feat(dates): add quarterOf
    Returns the calendar quarter (1-4) for a date.

--- src/dates/index.ts
+++ src/dates/index.ts
@@ -1,6 +1,7 @@
 export { getAge } from "./age";
 export { daysBetween } from "./daysBetween";
 export { formatDateAz, MONTH_NAMES_AZ } from "./months";
+export { quarterOf } from "./quarter";
 export { WEEKDAY_NAMES_AZ, weekdayNameAz } from "./weekdays";
 export { isWeekend, isWorkingDay } from "./workday";
 export { addWorkingDays, workingDaysBetween } from "./workdayCalc";
