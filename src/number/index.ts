5dbaae1bb71d265d9f6e7284d281376c7bfa9cc6
Murad Farajzada <mfarajzadaa@gmail.com>
2026-07-20 14:41:59 +0400

    feat(number): add isOdd (#98)

--- src/number/index.ts
+++ src/number/index.ts
@@ -3,6 +3,7 @@ export { clamp } from "./clamp";
 export { formatNumberAz } from "./format";
 export { inRange } from "./inRange";
 export { isEven } from "./isEven";
+export { isOdd } from "./isOdd";
 export { parseNumberAz } from "./parse";
 export { roundTo } from "./round";
 export { sum } from "./sum";
